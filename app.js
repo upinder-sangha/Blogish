c3d0de711fea1a6c3f1de800e1e5543cd2ca4848
Upinder <upindersangha01@gmail.com>
2020-04-06 16:53:02 +0530

    add database

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // jshint esversion:6
-
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -11,7 +11,21 @@ app.use(express.static("public"));
 
 
 
+mongoose.connect("mongodb://localhost:27017/blogWebsite", { useNewUrlParser: true, useUnifiedTopology: true });
 
+const blogSchema = new mongoose.Schema({
+	title: String,
+	body: String
+});
+const userSchema = new mongoose.Schema({
+	firstName: String,
+	lastName: String,
+	email: String,
+	password: String,
+	blogs: [blogSchema]
+});
+const Blog = mongoose.model("Blog", blogSchema);
+const User = mongoose.model("User", userSchema);
 
 
 
@@ -19,13 +33,38 @@ app.get("/", function (req, res) {
 	res.render("home");
 });
 
+app.post("/login", function (req, res) {
 
-app.get("/search", function (req, res) {
-	res.render("search");
 });
+
+app.post("/register", function (req, res) {
+
+});
+
+
+app.route("/search")
+	.get(function (req, res) {
+		res.render("search");
+	})
+
+	.post(function (req, res) {
+		res.redirect("/search");
+	});
+
 
 app.get("/user", function (req, res) {
 	res.render("user");
+});
+
+app.post("/compose", function (req, res) {
+		const blog = new Blog({
+			title: req.body.blogTitle,
+			body: req.body.blogBody
+		});
+		User.findOneAndUpdate({ firstName: "abc" }, { $push: { blogs: blog } }, { useFindAndModify: false }, function (err, user) {
+			if (!err)
+				res.redirect("/user");
+		});
 });
 
 
